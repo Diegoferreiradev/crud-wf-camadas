139f60246de101b104d3986c1485db76e30ab21e
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-06-22 16:00:53 -0300

    Classe Funcionario

---
+++ crud_wf_camadas/Model/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿namespace crud_wf_camadas.Model
+{
+    public class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
+
